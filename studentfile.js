c3dc77e1405cd150f6e05485476e8b59fc98416c
eden54514 <112902624+eden54514@users.noreply.github.com>
2022-09-21 22:48:49 -0500

    Create studentfile.js

---
+++ studentfile.js
@@ -0,0 +1,13 @@
+let student = function (){
+    let name = "eden";
+    let id =111;
+    return {
+        sName: name,
+        sId : id
+    }
+}();
+
+module.exports = student;
+
+
+// console.log('inside studentfile  '+ student.sName + ', '+student.sId);
